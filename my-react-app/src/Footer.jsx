4791b37fbd930fbb31e622c07e3ad206aa5abd1c
josia.schweizer <josia.schweizer@gmail.com>
2024-05-30 15:17:11 +0200

    added header, footer and food

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My React App</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header;
