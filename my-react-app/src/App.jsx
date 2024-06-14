ed4ea99e3460f15e21ae992245712a5095255499
josia.schweizer <josia.schweizer@gmail.com>
2024-06-14 15:50:44 +0200

    Gruss von Nils, Zixin und Laurin

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import Card from './Card.jsx'
+import Button from './Button/Button.jsx'
 
 function App() {
   return (
     <>
       <Card/>
-      <Card/>
-    </>
+      <Button/>
+    </> 
   );
 }
 
---
+++ my-react-app/src/Button/Button.jsx
@@ -0,0 +1,7 @@
+import styles from './Button.module.css'
+
+function Button(){
+
+    return(<button className="{styles.button}">Click Me</button>)
+}
+export default Button;
