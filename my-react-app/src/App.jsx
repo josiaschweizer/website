a61225fe0b28580274b5b93c589a408a9f2da7a3
Josia Schweizer <josia.schweizer@gmail.com>
2024-06-19 18:08:24 +0200

    added props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,18 @@
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
+import Studen from './Student.jsx'
+import Student from './Student.jsx';
 
 function App() {
   return (
     <>
       <Card/>
       <Button/>
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student name="Larry"/>
     </> 
   );
 }
