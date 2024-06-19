97ce955b567833ff61b59b5cdd1af23584c8c0df
Josia Schweizer <josia.schweizer@gmail.com>
2024-06-19 17:33:19 +0200

    added inline and external button

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -1,7 +1,22 @@
 import styles from './Button.module.css'
 
-function Button(){
+function Button() {
 
-    return(<button className="{styles.button}">Click Me</button>)
+    const inlineStyles = {
+        backgroundColor: 'hsl(200, 100%, 50%)',
+        color: 'white',
+        padding: '10px 20px',
+        borderRadius: '5px',
+        border: 'none',
+        cursor: 'pointer'
+    }
+
+    return (
+        <>
+            <button style={inlineStyles}>Click Me</button>
+            <button className={styles.button}>Click Me</button>
+        </>
+    )
+
 }
 export default Button;
